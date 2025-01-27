35bf5e84c4fd7d26bfbff4494a3627ad4706a167
Варя <varya.kornilova.21@inbox.ru>
2025-01-27 11:51:35 +0300

    Добавьте файлы проекта.

---
+++ AutoService/DB/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService.DB
+{
+    internal class DBConnection
+    {
+        public static AutoService2025Entities AutoServiceEntities = new AutoService2025Entities();
+    }
+}
