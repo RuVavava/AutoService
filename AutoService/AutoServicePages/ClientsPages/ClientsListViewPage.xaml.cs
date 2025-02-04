3a6c5f97c510c0fe2367b3718eb07414c4be6633
Варя <varya.kornilova.21@inbox.ru>
2025-02-04 05:51:40 +0300

    Доделана страница с информацией о клиенте

--- AutoService/AutoServicePages/ClientsPages/ClientsListViewPage.xaml.cs
+++ AutoService/AutoServicePages/ClientsPages/ClientsListViewPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AutoService.AutoServiceWindowws.ClientsWindowws;
+using AutoService.AutoServiceWindowws.ServicesWindowws;
 using AutoService.DB;
 using System;
 using System.Collections.Generic;
@@ -26,14 +27,76 @@ namespace AutoService.AutoServicePages.ClientsPages
         public ClientsListViewPage()
         {
             InitializeComponent();
+
+            Refresh(0);
+
             clients = new List<Client>(DBConnection.AutoServiceEntities.Client.ToList());
             ClientsLV.ItemsSource = clients;
             this.DataContext = this;
         }
 
+        private void Refresh(int i)
+        {
+            var allClients = DBConnection.AutoServiceEntities.Client.ToList();
+            var filtered = allClients.AsQueryable();
+
+            var searchText = SearchTB.Text.ToLower();
+
+            if (!string.IsNullOrWhiteSpace(searchText))
+            {
+                filtered = filtered.Where(x => x.FirstName.ToLower().Contains(searchText) || x.LastName.ToLower().Contains(searchText) || x.Patronymic.ToLower().Contains(searchText));
+            }
+
+            ClientsLV.ItemsSource = filtered.ToList();
+            CountRecordTBL.Text = $"{filtered.Count()} из {allClients.Count}";
+        }
+
+        private void RefreshLV()
+        {
+            ClientsLV.ItemsSource = DBConnection.AutoServiceEntities.Client.ToList();
+
+        }
+
+        private void PART_ContentHost_TextChanged(object sender, TextChangedEventArgs e) //Поиск текста
+        {
+            Refresh(0);
+        }
+
         private void AddClientBTN_Click(object sender, RoutedEventArgs e)
         {
+            AddNewClientWindoww addNewClientWindoww = new AddNewClientWindoww();
+            addNewClientWindoww.ShowDialog();
+            Refresh(0);
+            RefreshLV();
+        }
 
+        private void EditCkientBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClientsLV.SelectedItem is Client client)
+            {
+                ClientsLV.SelectedItem = null;
+                EditClientWindoww editClientWindow = new EditClientWindoww();
+                editClientWindow.ShowDialog();
+            }
+        }
+
+        private void RemoveClientBTN_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InfoClientBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.DataContext is Client client)
+            {
+                Client selectedClient = client;
+
+                InfoClientWindoww infoClientWindow = new InfoClientWindoww(selectedClient);
+                infoClientWindow.ShowDialog();
+
+                Refresh(0);
+            }
+            RefreshLV();
         }
     }
 }
---
+++ AutoService/AutoServiceWindowws/ClientsWindowws/InfoClientWindoww.xaml.cs
@@ -0,0 +1,52 @@
+﻿using AutoService.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoService.AutoServiceWindowws.ClientsWindowws
+{
+    /// <summary>
+    /// Логика взаимодействия для InfoClientWindoww.xaml
+    /// </summary>
+    public partial class InfoClientWindoww : Window
+    {
+        public static List<Client> clients {  get; set; }
+        Client contextClient;
+        public InfoClientWindoww(Client clients)
+        {
+            InitializeComponent();
+            contextClient = clients;
+            Refresh();
+            this.DataContext = this;
+        }
+
+        public void Refresh()
+        {
+            Photo.Source = new BitmapImage(new Uri(contextClient.PhotoPath, UriKind.Relative));
+            FirstNameTB.Text = contextClient.FirstName;
+            LastNameTB.Text = contextClient.LastName;
+            PatronymicTB.Text = contextClient.Patronymic;
+            EmailTB.Text = contextClient.Email;
+            PhoneTB.Text = contextClient.Phone;
+            GenderTB.Text = contextClient.Gender.Name;
+            DateBHTB.Text = contextClient.Birthday.ToString();
+            DateRegTB.Text = contextClient.RegistrationDate.ToString();
+        }
+
+        private void OKBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
